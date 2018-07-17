09cefa140a90d70208dab74ffe1e0b1d0a4fe1c9
Cece Zinny <cwzinny@gmail.com>
2018-07-17 13:59:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end 
   
   get '/hometown' do 
-    "My hometown is Wellesley, Massachusetts."
+    "My hometown is Massachusetts."
   end 
   
   get '/favorite-song' do
